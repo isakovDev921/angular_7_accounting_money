b3c267e2024c7ecc3504a445f3fb3da407ef34a5
I.Isakov <igorisakovigor@gmail.com>
2019-05-16 16:53:11 +0300

    start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
+import {MenuItem} from 'primeng/api';  
 
 @Component({
   selector: 'wfm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'home-money';
+  Indiastate = [  
+    "Rajasthan",  
+    "UP",  
+    "Mp",  
+    "Delhi",  
+    "Goa",  
+    "Gurjat",  
+    "Punjab"  
+  ];  
+  Ausstate = [  
+  
+    "New South Wales",  
+    "Queensland",  
+    "South Australia",  
+    "Tasmania"  
+  ];  
+  Slstate = [  
+    "Kandy",  
+    "Galle",  
+    "Kegalle",  
+    "Mannar"  
+  ];  
 }
+
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { AccordionModule } from 'primeng/components/accordion/accordion';
+import { OrderListModule } from 'primeng/orderlist';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, AccordionModule, OrderListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
