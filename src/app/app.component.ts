94dfc5887da2078524e679f08772f6915766d4f3
I.Isakov <igorisakovigor@gmail.com>
2019-05-16 17:06:28 +0300

    можно работать

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import {AccordionModule} from 'primeng/accordion';     //accordion and accordion tab
-import {MenuItem} from 'primeng/api';  
+import {ButtonModule} from 'primeng/button';
 
 @Component({
   selector: 'wfm-root',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/button';
 
 import { AccordionModule } from 'primeng/components/accordion/accordion';
 import { OrderListModule } from 'primeng/orderlist';
@@ -11,7 +12,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
     AppComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, AccordionModule, OrderListModule
+    BrowserModule, BrowserAnimationsModule, AccordionModule, OrderListModule, ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
