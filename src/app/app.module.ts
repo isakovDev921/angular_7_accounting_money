3c2bcc82320a8f0264731d7d8988f4e13fde6fb6
I.Isakov <igorisakovigor@gmail.com>
2019-05-20 13:38:42 +0300

    создал компоненты

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
 
 import { AccordionModule } from 'primeng/components/accordion/accordion';
 import { OrderListModule } from 'primeng/orderlist';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, BrowserAnimationsModule, AccordionModule, OrderListModule, ButtonModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AccordionModule,
+    OrderListModule,
+    ButtonModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
